81f2be1d36f80376dada32fec229924a189b01c9
Frank Gerhardt <fg@gerhardtinformatics.com>
2016-06-05 03:50:47 +0200

    initial

---
+++ helper.py
@@ -0,0 +1,16 @@
+
+hello = "Hello, world."
+
+def foo():
+    message = "bar"  
+    return message     # breakpoint here
+
+def greeting():
+    return hello
+
+def set_greeting(s):
+    hello = s
+    
+def change_list(l):
+    l.append("bar")    
+    
